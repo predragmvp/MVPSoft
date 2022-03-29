431f813c14d05467eccb4670e369088ed4c2119b
po_darko_medenica <medenica@profitoptics.com>
2022-03-29 19:21:51 +0200

    @darko

--- coderush/Controllers/WebsiteController.cs
+++ coderush/Controllers/WebsiteController.cs
@@ -8,5 +8,25 @@ namespace coderush.Controllers
         {
             return View();
         }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Development()
+        {
+            return View();
+        }
+        public IActionResult QA()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
